9d4b9d5f5f6ebab71cd120ec4cdc1a6b4808b724
Somesh Kale <someshk3850@gmail.com>
2020-10-06 18:42:18 -0400

    Added Angular

---
+++ personal-budget/src/app/homepage/homepage.component.ts
@@ -0,0 +1,49 @@
+import { Component, AfterViewInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Chart } from 'chart.js';
+
+@Component({
+  selector: 'pb-homepage',
+  templateUrl: './homepage.component.html',
+  styleUrls: ['./homepage.component.scss']
+})
+export class HomepageComponent implements AfterViewInit {
+
+  public dataSource = {
+    datasets: [
+        {
+            data: [],
+            backgroundColor: [
+                '#ffcd56',
+                '#ff6384',
+                '#36a2eb',
+                '#fd6b19',
+            ]
+        }
+    ],
+    labels: []
+  };
+
+  constructor(private http: HttpClient) { }
+
+  ngAfterViewInit(): void {
+    this.http.get('http://localhost:3000/budget')
+    .subscribe((res: any) => {
+      for (let i = 0; i < res.myBudget.length; i++) {
+        this.dataSource.datasets[0].data[i] = res.myBudget[i].budget;
+        this.dataSource.labels[i] = res.myBudget[i].title;
+        this.createChart();
+    }
+    });
+  }
+  // tslint:disable-next-line: typedef
+  createChart() {
+    let ctx = document.getElementById('myChart');
+    let myPieChart = new Chart(ctx, {
+        type: 'pie',
+        data: this.dataSource
+    });
+}
+
+}
